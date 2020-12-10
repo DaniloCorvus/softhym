2713c835b8e4fedb23d174f4e68b2271ee479d52
daniloCorvus <danilo.grisalez@corvuslab.co>
2020-12-10 15:29:45 -0500

    stash

--- app/Http/Controllers/DevolucioncambioController.php
+++ app/Http/Controllers/DevolucioncambioController.php
@@ -6,6 +6,7 @@ use App\Models\Cambio;
 use App\Models\Devolucioncambio;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 use function GuzzleHttp\Promise\all;
 
@@ -39,14 +40,29 @@ class DevolucioncambioController extends Controller
      */
     public function store(Request $request)
     {
-
         $data = json_decode(request()->get('data'));
+        $q = DB::table('Cambio As c')
+            ->select(DB::raw('(c.Valor_Destino - IFNULL(dc.valor_recibido, 0 )) As venta_final'))
+            ->leftJoin('Devolucion_Cambios As dc', 'c.Id_Cambio', '=', 'dc.cambio_id')
+            ->Join('Moneda As mo', 'c.Moneda_Origen', '=', 'mo.Id_Moneda')
+            ->Join('Moneda As md', 'c.Moneda_Destino', '=', 'md.Id_Moneda')
+            ->whereDate('Fecha', Carbon::today())
+            ->where('Id_Cambio', $data->Id_Cambio)
+            ->first();
+
+        $cambio = Cambio::with('devolucioncambios')->where('Id_Cambio', $data->Id_Cambio)->first();
+
+        $saldo = $q->venta_final ? $q->venta_final : 0;
+
+        if ($saldo <  $data->Valor_Devolver ||  count($cambio->devolucioncambios) > 0) {
+            return response()->json(['codigo' => 'warning']);
+        }
 
         if (!isset($data->Observacion)) {
             $data->Observacion = '';
         }
 
-        $devolucion =     Devolucioncambio::create([
+        Devolucioncambio::create([
             'hora' => Carbon::now(),
             'cambio_id' => $data->Id_Cambio,
             'observacion' => ($data->Observacion) ? $data->Observacion : '',
